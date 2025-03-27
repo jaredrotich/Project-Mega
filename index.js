f76d7a0535731c670258d735ed56488431df6cc1
jared <jaredrotich3@gmail.com>
2025-03-27 18:30:47 +0300

    modified stles.css

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-//Array to hold cart items
-let cart = ["Home", "Cart", "Login"];
-
-//function to add items to the cart
-function addToCart(productId, productName) {
-    //create a product object
-    const product = {id: productId, name: productName };
-
-    //add product to the cart array
-    cart.push(product);
-
-    //allert the user that the product was added
-    alert(`${productName} has been added to your cart!`);
-
-    //log the cart to the console for now
-    console.log(cart);
-}
+// Function to load products into the page
+function loadProducts() {
+    const productContainer = document.getElementById("Product-container");
+  
+    products.forEach(product => {
+      productContainer.innerHTML += generateProductHTML(product);
+    });
+  }
+  
+  // Wait for the DOM to fully load before adding products
+  document.addEventListener('DOMContentLoaded', loadProducts);
+  
